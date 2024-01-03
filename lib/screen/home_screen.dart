d279792e00d6f43f5f993252a7eadc32a20b665a
jeong won <jw221356@naver.com>
2024-01-03 20:57:32 +0900

    뼈대작성

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Home Screen'),
+    );
+  }
+}
+
+class _DDay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('DDay Widget');
+  }
+}
+
